bfcb002d243a3c7a767e6169685fd394715e2e94
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-10-30 19:43:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+// const word = 'Привет как дела' 
+ 
+// const arr = word.split(" ") 
+ 
+// console.log(arr) 
+ 
+ 
+ 
+ 
+//_________________________ 
+ 
+ 
+ 
+ 
+// const word = 'яблоко,банан,груша,апельсин' 
+// const arr = word.split(",") 
+ 
+// console.log(arr) 
+ 
+ 
+//____________________________ 
+ 
+ 
+ 
+// const word =  ["Привет", "мир", "!"] 
+ 
+// const arr = word.join(" ") 
+ 
+// console.log(arr) 
+ 
+ 
+ 
+//_________________________________ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const word = 'hello' 
+ 
+// const arr = word.split("").revers().join("") 
+ 
+// console.log(arr) 
+ 
+ 
+ 
+ 
+//______________________________________ 
+ 
+ 
+ 
+// const text = "Каждое слово начинается с буквы"; 
+// const firstLetters = text.split(' ').map(word => word[0]); 
+// console.log(firstLetters); 
+ 
+ 
+ 
+//__________________________________________________ 
+ 
+ 
+ 
+// const text = "Привет как дела"; 
+// const result = text.split(' ').join('-'); 
+// console.log(result); 
+ 
+ 
+ 
+//________________________________________ 
+ 
+ 
+// const text = "JavaScript - это весело" 
+// const words = text.split(' ') 
+// const wordCount = words.length 
+// console.log(wordCount) 
+ 
+ 
+//_____________________________________ 
+ 
+ 
+// const text = "   Привет   как    дела   "; 
+// const result = text.split(' ').filter(word => word).join(' ').trim(); 
+// console.log(result); 
+ 
+ 
+//____________________________________________________ 
+ 
+ 
+ 
+// const numbers = [1, 2, 3, 4, 5]; 
+// const result = numbers.join(','); 
+// console.log(result); 
+ 
+ 
+ 
+ 
+//_________________________________________ 
+ 
+ 
+// const text = "1 2 3 4 5"; 
+// const numbers = text.split(' ').map(Number); 
+// console.log(numbers);
